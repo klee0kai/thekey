d2cc917756cb070fa56dc2367847b6d5f8526b6c
klee0kai <klee0kai@gmail.com>
2022-09-01 12:26:22 +0300

    req permissions

--- app/src/main/java/com/kee0kai/thekey/ui/login/LoginActivity.java
+++ app/src/main/java/com/kee0kai/thekey/ui/login/LoginActivity.java
@@ -2,6 +2,8 @@ package com.kee0kai.thekey.ui.login;
 
 import static com.kee0kai.thekey.App.DI;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +12,11 @@ import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.kee0kai.thekey.App;
 import com.kee0kai.thekey.R;
@@ -21,12 +26,20 @@ import com.kee0kai.thekey.navig.activity_contracts.FindStorageActivityContract;
 import com.kee0kai.thekey.ui.common.BaseActivity;
 import com.kee0kai.thekey.utils.arch.IRefreshView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class LoginActivity extends BaseActivity implements IRefreshView, View.OnClickListener {
+
+    private static final String[] PERMISSIONS = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     private final LoginPresenter presenter = DI.presenter().loginPresenter();
 
     private ActivityLoginBinding binding;
     private ActivityResultLauncher<String> findStorageLauncher;
+    private ActivityResultLauncher<String[]> reqPermLauncher;
 
     @Override
     public SecureType getSecType() {
@@ -42,6 +55,8 @@ public class LoginActivity extends BaseActivity implements IRefreshView, View.On
         presenter.init();
 
         findStorageLauncher = registerForActivityResult(new FindStorageActivityContract(), presenter::setStorage);
+        reqPermLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), ignore -> {
+        });
         binding.edPassw.setOnEditorActionListener((textView, actionId, keyEvent) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 login();
@@ -51,6 +66,12 @@ public class LoginActivity extends BaseActivity implements IRefreshView, View.On
         });
         binding.btLogin.setOnClickListener(this);
         binding.btChangeStorage.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPermissions();
     }
 
     @Override
@@ -82,6 +103,19 @@ public class LoginActivity extends BaseActivity implements IRefreshView, View.On
             return;
         }
         presenter.login(passw);
+    }
+
+    private void checkPermissions() {
+        for (String perm : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
+                boolean showRationale = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M && shouldShowRequestPermissionRationale(perm);
+                if (!showRationale) {
+                    // user also CHECKED "never ask again"
+                    Toast.makeText(this, R.string.approve_permissions, Toast.LENGTH_LONG).show();
+                } else reqPermLauncher.launch(PERMISSIONS);
+                return;
+            }
+        }
     }
 
 
