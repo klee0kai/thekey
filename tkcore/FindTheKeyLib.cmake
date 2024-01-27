5b206b1951d21ab05ae5359839eea407eda89a6e
klee0kai <klee0kai@gmail.com>
2024-01-27 23:08:15 +0400

    move to parent folder

--- tkcore/FindTheKeyLib.cmake
+++ tkcore/FindTheKeyLib.cmake
@@ -1,6 +1,6 @@
 
-include(${CMAKE_CURRENT_LIST_DIR}/cpp_lib/core/CMakeLists.txt)
-include(${CMAKE_CURRENT_LIST_DIR}/cpp_lib/find/CMakeLists.txt)
-include(${CMAKE_CURRENT_LIST_DIR}/cpp_lib/storage1/CMakeLists.txt)
-include(${CMAKE_CURRENT_LIST_DIR}/cpp_lib/storage2/CMakeLists.txt)
+include(${CMAKE_CURRENT_LIST_DIR}/core/CMakeLists.txt)
+include(${CMAKE_CURRENT_LIST_DIR}/find/CMakeLists.txt)
+include(${CMAKE_CURRENT_LIST_DIR}/storage1/CMakeLists.txt)
+include(${CMAKE_CURRENT_LIST_DIR}/storage2/CMakeLists.txt)
 
