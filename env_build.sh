3342214329792c9afda160e5ea3f9f5d1011f4e4
klee0kai <klee0kai@gmail.com>
2024-02-24 16:34:41 +0400

    run tests in test folder

--- env_build.sh
+++ env_build.sh
@@ -95,13 +95,16 @@ function build_term_app() {
 
    cd "$WORKSPACE"
 
-   cp tkcore/build/test/testk1/testk1 builds/${CUR_OS_UNAME}/testk1
-   cp tkcore/build/test/testk2/testk2 builds/${CUR_OS_UNAME}/testk2
-   cp tkcore/build/term/tkey builds/${CUR_OS_UNAME}/tkey
+   cp $WORKSPACE/tkcore/build/test/testk1/testk1 builds/${CUR_OS_UNAME}/testk1
+   cp $WORKSPACE/tkcore/build/test/testk2/testk2 builds/${CUR_OS_UNAME}/testk2
+   cp $WORKSPACE/tkcore/build/term/tkey builds/${CUR_OS_UNAME}/tkey
 
    #run tests
-   "./builds/${CUR_OS_UNAME}/testk1"
-   "./builds/${CUR_OS_UNAME}/testk2"
+  cd $WORKSPACE/tkcore/build/test/testk1/testk1
+  "./testk1"
+
+   cd $WORKSPACE/tkcore/build/test/testk2/testk2
+  "./testk2"
 
    cd "$WORKSPACE"
 }
