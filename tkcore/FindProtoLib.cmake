51497b7cde49ce4e2d1da28d279c247619550482
klee0kai <klee0kai@gmail.com>
2024-02-24 17:02:24 +0400

    find protobuf hint

--- tkcore/FindProtoLib.cmake
+++
@@ -1,9 +0,0 @@
-
-find_package(Protobuf REQUIRED)
-
-message(Protobuf_INCLUDE_DIRS = ${Protobuf_INCLUDE_DIRS})
-
-add_library(protobuf INTERFACE)
-set_target_properties(protobuf PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES ${Protobuf_INCLUDE_DIRS}
-)
