259faa09feadf906c81f9eca39b1ceda81dd6116
klee0kai <klee0kai@gmail.com>
2024-06-09 11:50:37 +0400

    accumulate ext

--- core/src/main/kotlin/com/github/klee0kai/thekey/core/utils/common/CollectionExt.kt
+++ core/src/main/kotlin/com/github/klee0kai/thekey/core/utils/common/CollectionExt.kt
@@ -2,3 +2,15 @@ package com.github.klee0kai.thekey.core.utils.common
 
 inline fun <T> Iterable<T>.runForEach(action: T.() -> Unit) =
     forEach { action.invoke(it) }
+
+inline fun <reified T> Iterable<T>.accumulate(action: (T, T) -> T): T? {
+    var last: T? = null
+    forEachIndexed { index, t ->
+        last = if (index > 0) {
+            action(last as T, t)
+        } else {
+            t
+        }
+    }
+    return last
+}
