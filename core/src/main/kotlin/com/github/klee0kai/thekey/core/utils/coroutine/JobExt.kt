d3da46e3500356e43a87d3debd08e8ea909a235e
klee0kai <klee0kai@gmail.com>
2024-06-23 23:09:04 +0400

    storage searching min duration

--- core/src/main/kotlin/com/github/klee0kai/thekey/core/utils/coroutine/JobExt.kt
+++ core/src/main/kotlin/com/github/klee0kai/thekey/core/utils/coroutine/JobExt.kt
@@ -6,9 +6,12 @@ import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 fun <T> CoroutineScope.asyncResult(
     context: CoroutineContext = EmptyCoroutineContext,
@@ -23,6 +26,14 @@ fun <T> CoroutineScope.asyncResult(
 fun emptyJob(): Job = Job().also { it.complete() }
 
 fun <T> completeAsync(result: T): Deferred<T> = CompletableDeferred(value = result)
+
+suspend fun <T> minDuration(duration: Duration, block: suspend () -> T): T {
+    val start = System.currentTimeMillis()
+    val r = block.invoke()
+    val left = duration - (System.currentTimeMillis() - start).milliseconds
+    if (left.isPositive()) delay(left)
+    return r
+}
 
 suspend inline fun <reified T> Deferred<T>.awaitSec(): T? = withTimeoutOrNull(1000) {
     await()
