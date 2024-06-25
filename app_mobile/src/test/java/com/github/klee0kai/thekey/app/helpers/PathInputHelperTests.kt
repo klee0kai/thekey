e1db9852c7fbada965e4198df96eff0114871920
klee0kai <klee0kai@gmail.com>
2024-06-25 19:46:17 +0400

    using mocked di config in unit tests

--- app_mobile/src/test/java/com/github/klee0kai/thekey/app/helpers/PathInputHelperTests.kt
+++ app_mobile/src/test/java/com/github/klee0kai/thekey/app/helpers/PathInputHelperTests.kt
@@ -3,14 +3,20 @@ package com.github.klee0kai.thekey.app.helpers
 import androidx.compose.ui.text.input.TextFieldValue
 import com.github.klee0kai.thekey.app.di.DI
 import com.github.klee0kai.thekey.app.di.debug.DebugDI
+import com.github.klee0kai.thekey.app.di.hardResetToPreview
+import com.github.klee0kai.thekey.core.utils.annotations.DebugOnly
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 
+@OptIn(DebugOnly::class)
 class PathInputHelperTests {
 
-    private val helper by lazy { DI.pathInputHelper() }
+    private val helper by lazy {
+        DI.hardResetToPreview()
+        DI.pathInputHelper()
+    }
 
     @Before
     fun init() {
