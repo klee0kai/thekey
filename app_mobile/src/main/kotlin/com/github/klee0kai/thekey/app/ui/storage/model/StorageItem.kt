518d5ed47236c0aaa0b8330625d1cb4977a4c98e
klee0kai <klee0kai@gmail.com>
2024-08-10 20:18:59 +0400

    dummy models

--- app_mobile/src/main/kotlin/com/github/klee0kai/thekey/app/ui/storage/model/StorageItem.kt
+++ app_mobile/src/main/kotlin/com/github/klee0kai/thekey/app/ui/storage/model/StorageItem.kt
@@ -4,15 +4,20 @@ import android.os.Parcelable
 import com.github.klee0kai.thekey.core.domain.model.ColorGroup
 import com.github.klee0kai.thekey.core.domain.model.ColoredNote
 import com.github.klee0kai.thekey.core.domain.model.ColoredOtpNote
+import com.github.klee0kai.thekey.core.domain.model.dummyLoaded
+import com.github.klee0kai.thekey.core.utils.annotations.DebugOnly
 import kotlinx.parcelize.Parcelize
+import kotlin.random.Random
 
 @Parcelize
 data class StorageItem(
     val note: ColoredNote? = null,
     val otp: ColoredOtpNote? = null,
-) : Parcelable
+) : Parcelable {
+    companion object
+}
 
-val StorageItem.id get() = note?.ptnote ?: otp?.ptnote
+val StorageItem.id get() = "${note?.ptnote}/${otp?.ptnote}"
 
 val StorageItem.group get() = note?.group ?: otp?.group ?: ColorGroup()
 
@@ -30,7 +35,15 @@ fun StorageItem.sortableFlatText(): String {
     return "$site-$login"
 }
 
-
 fun ColoredNote.storageItem() = StorageItem(note = this)
 
 fun ColoredOtpNote.storageItem() = StorageItem(otp = this)
+
+@DebugOnly
+fun StorageItem.Companion.dummy(): StorageItem {
+    return if (Random.nextBoolean()) {
+        StorageItem(note = ColoredNote.dummyLoaded())
+    } else {
+        StorageItem(otp = ColoredOtpNote.dummyLoaded())
+    }
+}
