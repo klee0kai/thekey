a162134dadb5b03c3a49a47a62406c348da4b570
klee0kai <klee0kai@gmail.com>
2024-09-07 22:49:25 +0400

    debug and release source sets is reusable at commercial and community builds

--- buildSrc/src/main/kotlin/Commercial.kt
+++ buildSrc/src/main/kotlin/Commercial.kt
@@ -25,20 +25,31 @@ fun <BuildFeaturesT : BuildFeatures,
 }
 
 fun AndroidSourceSet.commercialSourceSet() {
-    when (name) {
-        in listOf(BuildTypes.commercialDebug, BuildTypes.commercialRelease) -> {
-            kotlin.srcDirs("src/commercial/kotlin")
-            java.srcDirs("src/commercial/java")
-            res.srcDirs("src/commercial/res")
-            resources.srcDirs("src/commercial/resources")
-        }
+    if (name in listOf(BuildTypes.commercialDebug, BuildTypes.commercialRelease)) {
+        kotlin.srcDirs("src/commercial/kotlin")
+        java.srcDirs("src/commercial/java")
+        res.srcDirs("src/commercial/res")
+        resources.srcDirs("src/commercial/resources")
+    }
 
-        in listOf(BuildTypes.debug, BuildTypes.release) -> {
-            kotlin.srcDirs("src/community/kotlin")
-            java.srcDirs("src/community/java")
-            res.srcDirs("src/community/res")
-            resources.srcDirs("src/community/resources")
-        }
+    if (name in listOf(BuildTypes.debug, BuildTypes.release)) {
+        kotlin.srcDirs("src/community/kotlin")
+        java.srcDirs("src/community/java")
+        res.srcDirs("src/community/res")
+        resources.srcDirs("src/community/resources")
+    }
+
+    if (name in listOf(BuildTypes.commercialDebug)) {
+        kotlin.srcDirs("src/debug/kotlin")
+        java.srcDirs("src/debug/java")
+        res.srcDirs("src/debug/res")
+        resources.srcDirs("src/debug/resources")
+    }
+    if (name in listOf(BuildTypes.commercialRelease)) {
+        kotlin.srcDirs("src/release/kotlin")
+        java.srcDirs("src/release/java")
+        res.srcDirs("src/release/res")
+        resources.srcDirs("src/release/resources")
     }
 }
 
