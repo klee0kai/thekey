de663ded667208d2338752dce005af2f9a4ecaf9
klee0kai <klee0kai@gmail.com>
2024-09-15 20:34:17 +0400

    exclude screens with routing from paparazzi

--- app_mobile/src/test/java/com/github/klee0kai/thekey/app/paparazzi/navigationboard/SimpleBoardScreens.kt
+++ app_mobile/src/test/java/com/github/klee0kai/thekey/app/paparazzi/navigationboard/SimpleBoardScreens.kt
@@ -1,42 +1,10 @@
 package com.github.klee0kai.thekey.app.paparazzi.navigationboard
 
 import com.github.klee0kai.thekey.app.paparazzi.BasePaparazzi
-import com.github.klee0kai.thekey.app.ui.simpleboard.NavigationBoardContainerPreview
-import com.github.klee0kai.thekey.app.ui.simpleboard.NavigationBoardEmptyPreview
-import com.github.klee0kai.thekey.app.ui.simpleboard.NavigationBoardNoCurrentPreview
-import com.github.klee0kai.thekey.app.ui.simpleboard.NavigationBoardTabletPreview
 import com.github.klee0kai.thekey.app.ui.simpleboard.SimpleBoardPreview
 import org.junit.Test
 
 class SimpleBoardScreens : BasePaparazzi() {
-
-    @Test
-    fun navigationBoardContainerPreview() {
-        paparazzi.snapshot {
-            NavigationBoardContainerPreview()
-        }
-    }
-
-    @Test
-    fun navigationBoardNoCurrentPreview() {
-        paparazzi.snapshot {
-            NavigationBoardNoCurrentPreview()
-        }
-    }
-
-    @Test
-    fun navigationBoardEmptyPreview() {
-        paparazzi.snapshot {
-            NavigationBoardEmptyPreview()
-        }
-    }
-
-    @Test
-    fun navigationBoardTabletPreview() {
-        paparazzi.snapshot {
-            NavigationBoardTabletPreview()
-        }
-    }
 
     @Test
     fun storageNavigationBoardPreview() {
